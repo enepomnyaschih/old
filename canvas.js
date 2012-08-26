8f9e0f8f92ae101f55cb0dd42e9ba04b9e567072
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2012-08-26 15:07:57 +0700

    Removed log.

--- canvas.js
+++ canvas.js
@@ -83,10 +83,7 @@
 		//this.drawDrainingRay({ y1 : -Math.PI / 16, y2 : -Math.PI / 25, star: new World.Star(0, 0, new World.Features(1, 0, 0)), s: 80 });
 		
 		if (this.world.lastGravity)
-		{
-			console.log(this.world.lastGravity.drainingStars.length);
 			this.world.lastGravity.drainingStars.each(this.drawDrainingStar, this);
-		}
 		
 		this.context.restore();
 	},
