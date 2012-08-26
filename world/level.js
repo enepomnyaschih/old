b71b6279099ad7df7022e135d55e231155cffa38
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2012-08-26 13:41:43 +0700

    Refactored levels. Change levels. Added pause.

--- world/level.js
+++ world/level.js
@@ -1,4 +1,6 @@
-var Level = Class.extend({
+var Level = JW.Config.extend({
+	index : 0,
+	description : null, // Array of String
     dt : 1,
     kEnginePower : .2,
     kRotate : .2,
@@ -14,12 +16,46 @@ var Level = Class.extend({
     trailLifeTime : 80,
     screenSize : 600,
     starInScreenAmount : 4,
-
-    init : function(kStarToShipDrainProportion) {
-        this.kStarToShipDrainProportion = kStarToShipDrainProportion;
-    }
+	
+	init: function(config, index)
+	{
+		this._super(config);
+		this.index = index;
+	}
 });
 
-Level.levels = [new Level(.2), new Level(.1), new Level(.05), new Level(.025)];
+Level.levels = [
+	{
+		description : [
+			"Fly closely to the stars and",
+			"consume their power to improve",
+			"your starship. Save energy"
+		],
+		kStarToShipDrainProportion : .2
+	},
+	{
+		description : [
+			"Stars are shining colder"
+		],
+		kStarToShipDrainProportion : .1
+	},
+	{
+		description : [
+			"New galaxy brings new pain"
+		],
+		kStarToShipDrainProportion : .05
+	},
+	{
+		description : [
+			"Your destiny is eternal",
+			"search of the Homeworld"
+		],
+		kStarToShipDrainProportion : .025
+	}
+];
 
-Level.current = Level.levels[0];
+Level.getLevel = function(index) {
+	return new Level(Level.levels[Math.min(index, Level.levels.length - 1)], index);
+}
+
+Level.current = Level.getLevel(0);
